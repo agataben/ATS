6753fe82b40fb16fc884956b08ec05022506f0a8
agataben / Agata + Benvegna <Agataben23@gmail.com>
2025-09-10 15:19:37 +0200

    Remove the redundant fixed seed from "synthetic_data.py"

--- ats/synthetic_data.py
+++ ats/synthetic_data.py
@@ -6,8 +6,8 @@ import pytz
 import math
 from copy import deepcopy
 
-rnd.seed(123)
-np.random.seed(123)
+#rnd.seed(123)
+#np.random.seed(123)
     
 def generate_time_boundaries(time_interval='90D', starting_year=None, starting_month= None, starting_day=None, starting_hour=None, starting_minute=None):
 
