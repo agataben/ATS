922803352d10a4b9697d43300e90ec46b8201e71
Agata Benvegna <Agataben23@gmail.com>
2025-09-25 18:44:10 +0200

    Add file "evaluators.py"
    In this module the function "evaluate_anomaly_detector()" is defined.
    This function applies an instance af the "MinMaxAnomalyDetector" to a pandas.DataFrame
    and checks if the data points marked as anomalous are detected by the detector.
    The output of the function is a dict: the keys are the labels of the inserted anomalies
    and the elements are pandas.Series, containing the timestamp of the anomalous value and one boolean
    for each variable of the DataFrame. The bool is True if the anomaly has been detected for that variable, False otherwise.

---
+++ ats/evaluators.py
@@ -0,0 +1,33 @@
+from .anomaly_detectors import MinMaxAnomalyDetector
+
+
+def evaluate_anomaly_detector(anomaly_detector, evaluation_timeseries_df, synthetic=False):
+
+    if not isinstance(anomaly_detector,MinMaxAnomalyDetector):
+        raise ValueError('Only MinMaxAnomalyDetector is supported')
+
+    if 'anomaly_label' not in evaluation_timeseries_df.columns:
+        raise ValueError('The anomaly_label column is missing: it is necessary for evaluation')
+    # Series with anomaly labels of data pints
+    anomaly_labels = evaluation_timeseries_df['anomaly_label'] 
+
+    if synthetic:
+        no_flag_timeseries_df = evaluation_timeseries_df.drop(columns=['anomaly_label','effect_label'])
+    else:
+        no_flag_timeseries_df = evaluation_timeseries_df.drop(columns=['anomaly_label'])
+
+    evaluated_timeseries_df = anomaly_detector.apply(no_flag_timeseries_df)
+    evaluated_anomaly_flags = evaluated_timeseries_df.filter(like='_anomaly')
+    evaluation_results = {}
+
+    for anomaly_label,frequency in anomaly_labels.value_counts().items():
+
+        for raw in range(len(evaluated_timeseries_df)):
+
+            if anomaly_labels.iloc[raw] == anomaly_label:
+                # evaluation_results is a dict of series of bool
+                evaluation_results[anomaly_label] = evaluated_anomaly_flags.iloc[raw].isin([1])
+                break
+
+    return evaluation_results
+
