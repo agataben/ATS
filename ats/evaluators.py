11b485dfadc4494a0d2b11f2ec3e058fb2c4ef63
Agata Benvegna <Agataben23@gmail.com>
2025-09-25 21:25:26 +0200

    Fix the return type of the function "evaluate_anomaly_detector()"
    The return type is still a dict with anomalies as keys but now the values are pandas.DataFrame

--- ats/evaluators.py
+++ ats/evaluators.py
@@ -9,7 +9,7 @@ def evaluate_anomaly_detector(anomaly_detector, evaluation_timeseries_df, synthe
     if 'anomaly_label' not in evaluation_timeseries_df.columns:
         raise ValueError('The anomaly_label column is missing: it is necessary for evaluation')
     # Series with anomaly labels of data pints
-    anomaly_labels = evaluation_timeseries_df['anomaly_label'] 
+    anomaly_labels = evaluation_timeseries_df['anomaly_label']
 
     if synthetic:
         no_flag_timeseries_df = evaluation_timeseries_df.drop(columns=['anomaly_label','effect_label'])
@@ -25,8 +25,7 @@ def evaluate_anomaly_detector(anomaly_detector, evaluation_timeseries_df, synthe
         for raw in range(len(evaluated_timeseries_df)):
 
             if anomaly_labels.iloc[raw] == anomaly_label:
-                # evaluation_results is a dict of series of bool
-                evaluation_results[anomaly_label] = evaluated_anomaly_flags.iloc[raw].isin([1])
+                evaluation_results[anomaly_label] = evaluated_anomaly_flags.iloc[[raw]].isin([1])
                 break
 
     return evaluation_results
