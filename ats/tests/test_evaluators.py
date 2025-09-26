2e603cea353c0d652619abbe0e281010c2e5af54
Agata Benvegna <Agataben23@gmail.com>
2025-09-26 13:07:07 +0200

    Add a new test for the module "evaluators.py"
    The function "evaluate_anomaly_detector()" now  is tested on a synthetic humi-temp timeseries
    with univariate spike anomaly, genarated using the class "SyntheticHumiTempGenerator" in
    the "synthetic_data.py" module

--- ats/tests/test_evaluators.py
+++ ats/tests/test_evaluators.py
@@ -1,8 +1,11 @@
 from ..evaluators import evaluate_anomaly_detector
 from ..anomaly_detectors import MinMaxAnomalyDetector
+from ..synthetic_data import SyntheticHumiTempTimeseriesGenerator
 from ..utils import generate_timeseries_df
 import unittest
 import pandas as pd
+import random as rnd
+import numpy as np
 
 
 # Setup logging
@@ -10,6 +13,11 @@ from .. import logger
 logger.setup()
 
 class TestEvaluators(unittest.TestCase):
+
+    def setUp(self):
+
+        rnd.seed(123)
+        np.random.seed(123)
 
     def test_evaluate_anomaly_detector(self):
 
@@ -43,9 +51,34 @@ class TestEvaluators(unittest.TestCase):
         self.assertEqual(evaluation_results['anomaly_1'].loc['2025-06-10 14:00:00+00:00','value_anomaly'],True)
         self.assertEqual(evaluation_results['normal'].loc['2025-06-10 15:00:00+00:00','value_anomaly'],False)
 
+    def test_evaluate_anomaly_det_on_spiked_synth_timeseries(self):
 
+        spiked_humi_temp_generator = SyntheticHumiTempTimeseriesGenerator()
+        spiked_humi_temp_df = spiked_humi_temp_generator.generate(anomalies=['spike_uv'],effects=[])
+        # Generated DataFrame:
+        # Timestamp                    temperature           humidity             anomaly_label    effect_label
+        # ...
+        # 1973-05-07 11:02:00+00:00    24.761107302835810    50.63704719243785    normal           bare
+        # 1973-05-07 11:17:00+00:00    24.868377982941322    50.350992045489804   normal           bare
+        # 1973-05-07 11:32:00+00:00    24.944096137309916    50.14907696717356    normal           bare
+        # 1973-05-07 11:47:00+00:00    15.987937529180115    59.03216658885302    spike_uv         bare
+        # 1973-05-07 12:02:00+00:00    24.999714422981285    50.000761538716574   normal           bare
+        # 1973-05-07 12:17:00+00:00    24.979376388246145    50.05499629801029    normal           bare
+        # 1973-05-07 12:32:00+00:00    24.927010515561776    50.194638625168594   normal           bare
+        # ...
+        min_max_anomaly_detector = MinMaxAnomalyDetector()
+        evaluation_results = evaluate_anomaly_detector(min_max_anomaly_detector, spiked_humi_temp_df, synthetic=True)
+        self.assertEqual(len(evaluation_results),2)
+        self.assertIsInstance(evaluation_results,dict)
+        self.assertIsInstance(evaluation_results['spike_uv'],pd.DataFrame)
+        # The detector does not see the downward spike in temperature as anomalous because the min temperature
+        # value is 17.5.
+        # The detector does not see the upward spike in humidity as anomalous because the max humidity
+        # value is 70.
+        # Evaluation_results:
+        # { 'normal':         temperature_anomaly   humidity_anomaly
+        #               0     False                 False,
+        # 'spike_uv':         temperature_anomaly   humidity_anomaly
+        #               474   False                  False
+        # }
 
-
-
-
-  
