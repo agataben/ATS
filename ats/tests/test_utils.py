1887cfcf7ea3a3bc3902716ddda3e2c3050fc29f
clarasaia <clara.saia.1997@gmail.com>
2025-09-26 16:34:28 +0200

    changes language to english for all the normalization fuctions. Exludes print() that must be removed

--- ats/tests/test_utils.py
+++ ats/tests/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-from ..utils import generate_timeseries_df, normalizza_parametro, normalizzazione_df
+from ..utils import generate_timeseries_df, normalize_parameter, normalize_df
 
 # Setup logging
 from .. import logger
@@ -15,27 +15,27 @@ class TestUtils(unittest.TestCase):
         timeseries_df = generate_timeseries_df(entries=10, variables=2)
         self.assertEqual(timeseries_df.shape, (10,2))
 
-    # Tests for normalizza_parametro
-    def test_normalizza_parametro_vario(self):
+    # Tests for normalize_parameter
+    def test_normalize_generic_parameter(self):
         df = pd.DataFrame({"a": [1, 2, 3, 4, 5]})
-        normalized = normalizza_parametro(df, "a")
+        normalized = normalize_parameter(df, "a")
         expected = pd.Series([0.0, 0.25, 0.5, 0.75, 1.0], name="a")
         #È una funzione di pandas che confronta due pd.Series e lancia un errore se non sono identiche
         pd.testing.assert_series_equal(normalized.reset_index(drop=True), expected)
 
-    def test_normalizza_parametro_costante(self):
+    def test_normalize_costant_parameter(self):
         df = pd.DataFrame({"b": [7, 7, 7]})
-        normalized = normalizza_parametro(df, "b")
+        normalized = normalize_parameter(df, "b")
         expected = pd.Series([1, 1, 1], name="b")
         pd.testing.assert_series_equal(normalized.reset_index(drop=True), expected)
     
-    # Tests for normalizzazione_df
+    # Tests for normalize_df
     def test_normalize_all_columns(self):
         df = pd.DataFrame({
             "a": [1, 2, 3, 4, 5],
             "b": [10, 20, 30, 40, 50]
         })
-        df_norm = normalizzazione_df(df)
+        df_norm = normalize_df(df)
         expected = pd.DataFrame({
             "a_norm": [0.0, 0.25, 0.5, 0.75, 1.0],
             "b_norm": [0.0, 0.25, 0.5, 0.75, 1.0]
@@ -49,15 +49,15 @@ class TestUtils(unittest.TestCase):
             "context": [True, False, True],
             "name": ["x", "y", "z"]
         })
-        df_norm = normalizzazione_df(df,parameters_subset=["a"])
+        df_norm = normalize_df(df,parameters_subset=["a"])
         expected = pd.DataFrame({
             "a_norm": [0.0, 0.5, 1],
         })
         pd.testing.assert_frame_equal(df_norm, expected)
 
-        df_norm = normalizzazione_df(df,parameters_subset=["a","context"])
+        df_norm = normalize_df(df,parameters_subset=["a","context"])
         pd.testing.assert_frame_equal(df_norm, expected)
 
-        df_norm = normalizzazione_df(df,parameters_subset=["a","context","name"])
+        df_norm = normalize_df(df,parameters_subset=["a","context","name"])
         pd.testing.assert_frame_equal(df_norm, expected)
 
