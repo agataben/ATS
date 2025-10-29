d02a4302e6dad1a98bf9a008a3c0960104ce6949
clarasaia <clara.saia.1997@gmail.com>
2025-10-29 15:02:28 +0100

    implements generate_reference_dat

--- ats/generator.py
+++ ats/generator.py
@@ -1,26 +1,45 @@
 from .synthetic_data import SyntheticHumiTempTimeseriesGenerator
+import random as rnd
 
 class EvaluationDataGenerator():
     pass
 
 class HumiTempEvaluationDataGenerator(EvaluationDataGenerator):
 
-    def __init__(self, howmany_series,# Series_lenght è ridodante
+    def __init__(self, howmany_dataset,# Series_lenght è ridodante
                  temperature=True, humidity=True,
                  sampling_interval= '15min',
                  observation_window='30D'):
-        self.howmany_series = howmany_series
+        self.howmany_dataset = howmany_dataset
         self.temperature = temperature
         self.humidity = humidity
         self.sampling_interval = sampling_interval
         self.observation_window = observation_window
 
-    def generate_reference_data(self, dataset_reference_size=3, effect_type=[]):
-        generator = SyntheticHumiTempTimeseriesGenerator()
-        reference_data = generator.generate(dataset_reference_size, 
-                                            effect_type)
+    def generate_reference_data(self, howmany_dataset=3):
+        # It wuold be nice to have a fuction of syntetic data to achieve theese:
+        aviable_effects = ['noise', 'seasons', 'clouds']
+        reference_dataset = []
 
-        return reference_data
+        generator = SyntheticHumiTempTimeseriesGenerator(
+                sampling_interval=self.sampling_interval,
+                observation_window=self.observation_window,
+                temperature=self.temperature,
+                humidity=self.humidity
+        )
+
+        for i in range(howmany_dataset):
+            n_effects = rnd.randint(0, len(available_effects))
+            chosen_effects = rnd.sample(available_effects, n_effects)
+            
+            reference_data = generator.generate(effects=chosen_effects
+                                                anomalies = None, 
+                                                plot = False, 
+                                                generate_csv = False)
+            reference_dataset.append(reference_data)
+
+
+        return reference_dataset
 
     #def generate_test_data(self, dataset_test_size, effect_type=[]):
        # generator = SyntheticHumiTempTimeseriesGenerator()
