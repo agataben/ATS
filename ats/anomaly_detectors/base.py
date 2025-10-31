4ec40957f3378331234b6fe36c9d2b966a8b9c18
Stefano Alberto Russo <stefano.russo@gmail.com>
2025-10-31 18:32:29 +0100

    Changed the interface of the base anomlay detector to expect lists of DataFrames instead of sets (See #36)

--- ats/anomaly_detectors/base.py
+++ ats/anomaly_detectors/base.py
@@ -15,7 +15,7 @@ class AnomalyDetector():
         Fit the anomaly detector on some (time series) data.
 
         Args:
-            data (pd.DataFrame or set[pd.DataFrame]): A single time series (as a pandas DataFrame) or a set of time series (each as a pandas DataFrame).
+            data (pd.DataFrame or list[pd.DataFrame]): A single time series (as a pandas DataFrame) or a list of time series (each as a pandas DataFrame).
             The index of the each DataFrame must be named "timestamp", and each column should represents a variable. 
         """
         raise NotImplementedError()
@@ -26,11 +26,11 @@ class AnomalyDetector():
         Apply the anomaly detector on some (time series) data.
 
         Args:
-            data (pd.DataFrame or set[pd.DataFrame]): A single time series (in pandas DataFrame format) or a set of time series (in pandas DataFrame format).
+            data (pd.DataFrame or list[pd.DataFrame]): A single time series (in pandas DataFrame format) or a list of time series (in pandas DataFrame format).
             The index of the data frame(s) must be named "timestamp", and each column is supposed to represents a variable.
 
         Returns:
-            pd.DataFrame or set[pd.DataFrame]: the input data with the "anomaly" flag and optional "anomaly_score" columns added.
+            pd.DataFrame or list[pd.DataFrame]: the input data with the "anomaly" flag and optional "anomaly_score" columns added on the time series.
         """
         raise NotImplementedError()
 
